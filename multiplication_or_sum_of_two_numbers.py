646bb0b7baab07eb19c34b8e9604c156e1299e98
veronbungag <rveron.bungag@gmail.com>
2024-01-23 21:10:43 +0800

    define the two numbers

--- multiplication_or_sum_of_two_numbers.py
+++ multiplication_or_sum_of_two_numbers.py
@@ -1,4 +1,5 @@
 #define the two numbers for multiplication and sum
+def multiplication_or_sum(first_number, second_number):
 #calculate product and sum of first number and second number
 #check if product is less than 1000
 
